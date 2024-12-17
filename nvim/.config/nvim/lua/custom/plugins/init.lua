aa1c041f4e320746d04c826d86a569aab086eaf9
rbrady98 <rian@doit.com>
2024-12-17 17:21:38 +0000

    feat(nvim): add snacks dashboard

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -34,4 +34,22 @@ return {
     'windwp/nvim-ts-autotag',
     config = true,
   },
+
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      dashboard = {
+        sections = {
+          { section = 'header' },
+          { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
+          { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+          { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+          { section = 'startup' },
+        },
+      },
+    },
+  },
 }
